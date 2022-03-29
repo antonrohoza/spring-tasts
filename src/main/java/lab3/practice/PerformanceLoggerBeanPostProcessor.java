503b75c2144631950363649fbd2a33862763429c
arohoza <anton.rohoza@gmail.com>
2022-03-29 14:15:17 +0300

    fix lab3

--- src/main/java/lab3/practice/PerformanceLoggerBeanPostProcessor.java
+++ src/main/java/lab3/practice/PerformanceLoggerBeanPostProcessor.java
@@ -17,7 +17,10 @@ public class PerformanceLoggerBeanPostProcessor implements BeanPostProcessor {
     Class<?> beanClass = bean.getClass();
     Logger annotationLogger = beanClass.getAnnotation(Logger.class);
     Transactional annotationTransactional = beanClass.getAnnotation(Transactional.class);
-    if (annotationLogger != null && annotationTransactional != null) {
+    if (annotationLogger != null) {
+      map.put(beanName, beanClass);
+    }
+    if (annotationTransactional != null) {
       map.put(beanName, beanClass);
     }
     return bean;
@@ -29,27 +32,45 @@ public class PerformanceLoggerBeanPostProcessor implements BeanPostProcessor {
     if (clazz == null) {
       return bean;
     }
-    Object proxyLoggerObject = getLoggerProxy(clazz, bean);
-    return getTransactionalProxy(clazz, proxyLoggerObject);
+
+    Logger annotationLogger = clazz.getAnnotation(Logger.class);
+    Transactional annotationTransactional = clazz.getAnnotation(Transactional.class);
+
+    if (annotationLogger != null && annotationTransactional != null) {
+      Object proxyLoggerObject = getLoggerProxy(clazz, bean);
+      return getTransactionalProxy(clazz, proxyLoggerObject);
+    } else if (annotationLogger != null) {
+      return getLoggerProxy(clazz, bean);
+    } else if (annotationTransactional != null) {
+      return getTransactionalProxy(clazz, bean);
+    } else {
+      return bean;
+    }
   }
 
   private Object getLoggerProxy(Class<?> clazz, Object bean) {
-    return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(),
-                                  (proxy, method, args) -> {
-                                    long start = System.currentTimeMillis();
-                                    Object value = ReflectionUtils.invokeMethod(method, bean, args);
-                                    System.out.println("Execution took = " + (System.currentTimeMillis() - start));
-                                    return value;
-                                  });
+    return Proxy.newProxyInstance(clazz.getClassLoader(),
+                                  clazz.getInterfaces(),
+                                  (proxy, method, args) -> getLoggerHandler(bean, method, args));
+  }
+
+  private Object getLoggerHandler(Object bean, java.lang.reflect.Method method, Object[] args) {
+    long start = System.currentTimeMillis();
+    Object value = ReflectionUtils.invokeMethod(method, bean, args);
+    System.out.println("Execution took = " + (System.currentTimeMillis() - start));
+    return value;
   }
 
   private Object getTransactionalProxy(Class<?> clazz, Object bean) {
-    return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(),
-                                  (proxy, method, args) -> {
-                                    System.out.println("Transation STARTED");
-                                    Object value = ReflectionUtils.invokeMethod(method, bean, args);
-                                    System.out.println("Transaction FINISHED");
-                                    return value;
-                                  });
+    return Proxy.newProxyInstance(clazz.getClassLoader(),
+                                  clazz.getInterfaces(),
+                                  (proxy, method, args) -> getTransactionalHandler(bean, method, args));
+  }
+
+  private Object getTransactionalHandler(Object bean, java.lang.reflect.Method method, Object[] args) {
+    System.out.println("Transation STARTED");
+    Object value = ReflectionUtils.invokeMethod(method, bean, args);
+    System.out.println("Transaction FINISHED");
+    return value;
   }
 }
