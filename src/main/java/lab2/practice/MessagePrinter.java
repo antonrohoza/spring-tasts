a276699ac79c60f1ce89f5321b18b982bf79cb19
arohoza <anton.rohoza@gmail.com>
2022-03-29 13:51:33 +0300

    fix lab2

--- src/main/java/lab2/practice/MessagePrinter.java
+++ src/main/java/lab2/practice/MessagePrinter.java
@@ -3,10 +3,8 @@ package lab2.practice;
 import lab2.example.DeprecatedClass;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 @DeprecatedClass(UpdatedMessagePrinter.class)
-@Component
 @Setter
 @Getter
 public class MessagePrinter implements Printer {
--- src/main/java/lab2/practice/PropertyPlaceholder.java
+++ src/main/java/lab2/practice/PropertyPlaceholder.java
@@ -1,29 +1,51 @@
 package lab2.practice;
 
-import lab2.example.DeprecatedClass;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Properties;
+import lombok.SneakyThrows;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class PropertyPlaceholder implements BeanFactoryPostProcessor {
 
   public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
       throws BeansException {
-    for (String beanName : beanFactory.getBeanDefinitionNames()) {
-      BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
-      String currentClassName = beanDefinition.getBeanClassName();
-      try {
-        Class<?> clazz = Class.forName(currentClassName);
-        DeprecatedClass clazzAnnotation = clazz.getAnnotation(DeprecatedClass.class);
-        if (clazzAnnotation != null) {
-          beanDefinition.setBeanClassName(clazzAnnotation.value().getCanonicalName());
-        }
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      }
+    Properties properties = getProperties();
+    Arrays.stream(beanFactory.getBeanDefinitionNames())
+          .map(beanFactory::getBeanDefinition)
+          .map(BeanDefinition::getPropertyValues)
+          .forEach(mutablePropertyValues -> redefineValues(0, mutablePropertyValues, properties));
+  }
+
+  private void redefineValues(int counter, MutablePropertyValues propertyValues, Properties properties) {
+    if (counter < propertyValues.size()) {
+      PropertyValue propertyValue = propertyValues.getPropertyValueList().get(counter);
+      String originalPropertyValue = Objects.requireNonNull(propertyValue.getValue()).toString();
+      String propertyName = parsePropertyName(originalPropertyValue);
+      String realPropertyValue = properties.getProperty(propertyName);
+      propertyValues.add(propertyValue.getName(), realPropertyValue);
+      redefineValues(++counter, propertyValues, properties);
     }
+  }
+
+  private String parsePropertyName(String propertyValueName) {
+    return Arrays.stream(propertyValueName.split("\\$\\{"))
+                 .filter(element -> element.contains("}"))
+                 .map(element -> element.split("}")[0])
+                 .findFirst()
+                 .orElseThrow(RuntimeException::new);
+  }
+
+  @SneakyThrows
+  private Properties getProperties() {
+    Properties properties = new Properties();
+    properties.load(
+        PropertyPlaceholder.class.getClassLoader().getResourceAsStream("application.properties"));
+    return properties;
   }
 }
